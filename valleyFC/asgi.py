e58f97f81d18f2f198bc8a92135f47256f2dc9ac
PhredE <phrede@PhredEs-MacBook-Air.local>
2022-02-22 16:09:02 -0800

    initial commit

---
+++ valleyFC/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for valleyFC project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'valleyFC.settings')
+
+application = get_asgi_application()
