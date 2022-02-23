86ccdc4a4c68170bc8eac3a1d57a85edbd400cb6
PhredE <phrede@PhredEs-MacBook-Air.local>
2022-02-22 17:06:36 -0800

    views, urls, added

--- main_app/views.py
+++ main_app/views.py
@@ -6,3 +6,6 @@ from django.http import HttpResponse
 # Define the home view
 def home(request):
   return HttpResponse('<h1>Valley FC is #1</h1>')
+
+def about(request):
+  return render(request, 'about.html')
